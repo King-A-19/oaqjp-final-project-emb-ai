2860446d555e0f4b145d00567c78507f7a6225c9
King-A-19 <godatbelecof@gmail.com>
2024-08-07 12:27:01 -0400

    Updated code with latest changes

--- server.py
+++ server.py
@@ -1,25 +1,28 @@
 # Import Flask, render_template, request from the flask framework package
-from flask import Flask, render_template, request
+from flask import Flask, request, jsonify, render_template
 
+# Import the emotion_detector function from the EmotionDetection module
 from EmotionDetection.emotion_detection import emotion_detector
 
-#Initiate the flask app 
+# Initiate the Flask app
 app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
-def sent_analyzer():
-    # Retrieve the text to analyze from the request arguments
-    text_to_analyze = request.args.get('textToAnalyze')
+def analyze_emotion():
+    # Retrieve the text to analyze from the query parameters
+    text_to_analyse = request.args.get('textToAnalyze')
 
-    # Pass the text to the sentiment_analyzer function and store the response
-    response = emotion_detector(text_to_analyze)
+    # Call the emotion_detector function to analyze the text
+    response = emotion_detector(text_to_analyse)
 
-    # Extract the info
-    emotions = response_dict['emotionPredictions'][0]['emotion']
-    dominant_emotion = max(emotions, key=emotions.get)
-
-   
-    return "For the given statement, the system response is {} The dominant emotion is  {}.".format(emotions.split('_')[1], dominant_emotion)
+    # Prepare the response in the specified format
+    return (f"For the given statement, the system response is "
+            f"'anger': {response['anger']}, "
+            f"'disgust': {response['disgust']}, "
+            f"'fear': {response['fear']}, "
+            f"'joy': {response['joy']}, and "
+            f"'sadness': {response['sadness']}. "
+            f"The dominant emotion is {response['dominant_emotion']}.")
 
 @app.route("/")
 def render_index_page():
