5b6354bb9d702e42a5eb9c1aae4fea2b4d3b1549
King-A-19 <godatbelecof@gmail.com>
2024-08-08 13:46:29 -0400

    Updated code with latest changes

--- server.py
+++ server.py
@@ -1,10 +1,17 @@
-from flask import Flask, request, jsonify, render_template
+"""
+This module creates a Flask web application that detects emotions in text.
+"""
+
+from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def analyze_emotion():
+    """
+    Analyzes the emotion of the text provided as a query parameter.
+    """
     text_to_analyse = request.args.get('textToAnalyze')
 
     # Check if the input text is blank or None
@@ -16,16 +23,22 @@ def analyze_emotion():
     if response['dominant_emotion'] is None:
         return 'Invalid text! Please try again.', 200
 
-    return (f"For the given statement, the system response is "
-            f"'anger': {response['anger']}, "
-            f"'disgust': {response['disgust']}, "
-            f"'fear': {response['fear']}, "
-            f"'joy': {response['joy']}, and "
-            f"'sadness': {response['sadness']}. "
-            f"The dominant emotion is {response['dominant_emotion']}.")
+    return (
+        "For the given statement, the system response is "
+        f"'anger': {response['anger']}, 'disgust': {response['disgust']}, "
+        f"'fear': {response['fear']}, 'joy': {response['joy']}, and "
+        f"'sadness': {response['sadness']}. The dominant emotion is "
+        f"{response['dominant_emotion']}."
+    )
 
 @app.route("/")
 def render_index_page():
+    """
+    Renders the index page of the web application.
+
+    Returns:
+        str: The rendered HTML content of the index page.
+    """
     return render_template('index.html')
 
 if __name__ == "__main__":
